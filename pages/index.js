104569e17f1df1b39ef71d635b737785fa86c18c
wakawaka <89671440+wakawakathedev@users.noreply.github.com>
2021-11-06 12:00:46 +0800

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState, useRef } from "react";
 import Head from "../src/components/Head"
 import QRCode from "react-qr-code";
 
@@ -6,67 +6,111 @@ import styles from '../styles/Home.module.css';
 import { Account } from "thenewboston";
 
 const useIsOnline = () => {
-  if (typeof window !== 'undefined') {
-    return window.navigator.onLine
-  }
-  return true
+  const [isOnline, setIsOnline] = useState(false)
+
+  const setOffline = () => setIsOnline(false)
+  const setOnline = () => setIsOnline(true)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener("offline", setOffline)
+      window.addEventListener("online", () => setOnline)
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener("offline", setOffline)
+        window.removeEventListener("online", () => setOnline)
+      }
+    }
+  }, [isOnline])
+
+  return isOnline
 }
+
 
 export default function Home() {
   const isOnline = useIsOnline()
 
-  const [mousePosition, setMousePosition] = useState(undefined)
+  const [isLocked, toggleLock] = useState(false)
   const [accountNumber, setAccountNumber] = useState('')
 
   const [privateKey, setPrivateKey] = useState('')
 
-  const generateAccount = () => {
+  const AccountAreaRef = useRef(null)
+
+  const generateAccount = useCallback(() => {
+    if (isLocked) return
+    console.log(isLocked, 'here')
     const account = new Account()
     setAccountNumber(account.accountNumberHex)
     setPrivateKey(account.signingKeyHex)
-  }
+  }, [isLocked])
 
   const handleMouseMove = useCallback((e) => {
-    setMousePosition(`x: ${e.x}, y: ${e.y}`)
-    if (!isOnline) generateAccount()
-  }, [isOnline])
+    if (isLocked) return
+    else if (!isOnline) generateAccount()
+  }, [isOnline, isLocked])
 
+  // on Mount
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window?.addEventListener('mousemove', handleMouseMove)
+    if (typeof window !== 'undefined' && !isOnline && !isLocked) {
+      AccountAreaRef?.current?.addEventListener('mousemove', handleMouseMove)
     }
     if (!isOnline) generateAccount()
-  }, [handleMouseMove, isOnline])
+  }, [isOnline, handleMouseMove, isLocked])
+
+  const lockAccount = useCallback(() => {
+    toggleLock(true)
+  }, [toggleLock])
 
   return (
     <div className={styles.container}>
       <Head />
-      <h1 className="hide-print">Online Status: {isOnline ? "You are Online" : "You are Offline"}</h1>
+      <div className="hide-print">
 
-      {isOnline && (
-        <div className="hide-print">Please go offline</div>
-      )}
+        <h1>Online Status: {isOnline ? "You are Online" : "You are Offline"}</h1>
 
-      <div>
-        <>
-          {accountNumber && (
-            <>
-              <h1>Public Key</h1>
-              <h2>Account Number / Deposit / Verify</h2>
-              <p>{accountNumber}</p>
-              <QRCode value={accountNumber} />
-            </>
-          )}
+        <p>Origami is a paper wallet generator app to be used when the browser is offline.</p>
+        <p>The user can generate custom paper wallets which can be printed offline.</p>
 
-          {privateKey && (
-            <>
-              <h1>Private Key</h1>
-              <h2>Withdraw / Spend</h2>
-              <QRCode value={privateKey} />
-            </>
-          )}
-        </>
+        {isOnline && (
+          <div className="alert warning">
+            <p>Please go offline</p>
+          </div>
+        )}
       </div>
+
+      {!isOnline && (
+        <div>
+          <button className="hide-print" onClick={lockAccount}>Lock Account {isLocked}</button>
+          <p>{isLocked.toString()}</p>
+
+          <div ref={AccountAreaRef}>
+            {accountNumber && (
+              <>
+                <h1>Public Key</h1>
+                <h2>Account Number / Deposit / Verify</h2>
+                <div>
+                  <p style={{ width: 200, fontSize: 14, lineBreak: "anywhere" }}>{accountNumber}</p>
+                </div>
+                <QRCode size={128} value={accountNumber} />
+              </>
+            )}
+
+            {privateKey && (
+              <>
+                <h1>Private Key</h1>
+                <h2>Withdraw / Spend</h2>
+                <div>
+                  <p style={{ width: 200, fontSize: 14, lineBreak: "anywhere" }}>{privateKey}</p>
+                </div>
+                <QRCode size={128} value={privateKey} />
+              </>
+            )}
+          </div>
+
+        </div>)}
     </div>
   );
 }
