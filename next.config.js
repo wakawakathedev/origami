973918207daf509a1af5ad093b0974dbca1bd38f
wakawaka <89671440+wakawakathedev@users.noreply.github.com>
2021-11-09 07:04:12 +0800

    ADD: hotfix for nextjs deployment;

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
+    // [issue] https://github.com/shadowwalker/next-pwa/issues/288
+    buildExcludes: [/middleware-manifest.json$/]
   },
   reactStrictMode: true,
 });
